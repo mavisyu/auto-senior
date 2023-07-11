865847bfff220cc12d49c55158c53e30d2e3bfa4
mavisyu <monica0116501@gmail.com>
2023-07-11 18:32:22 +0800

    add task-2

--- 1_score.py
+++ 1_score.py
@@ -1,7 +1,12 @@
-score = ['35', 46, '57', 91, '29']
+# score = ['35', 46, '57', 91, '29']
 correct_score_list = []
 
+print('Enter your score: ')
+enter_score = input()
+score = list(map(int,enter_score.lstrip('[').rstrip(']').split(',')))
+print(f'wrong score is : {score}')
+
 for index, x in enumerate(score):
-  correct_score = str(score[index])[::-1]
-  correct_score_list.append(correct_score)
-print(correct_score_list)
+    correct_score = str(score[index])[::-1]
+    correct_score_list.append(correct_score)
+print(f'correct score is : {correct_score_list}')
---
+++ 2_letter.py
@@ -0,0 +1,14 @@
+import collections
+
+letter = "Hello welcome to Cathay 60th year anniversary"
+
+# letter_upper = letter.upper().replace(" ", "")
+# print(sorted(collections.Counter(letter_upper).items()))
+
+count = {}
+for x in letter.upper().replace(" ", ""):
+    if x not in count:
+        count[x] = 1
+    else:
+        count[x] += 1
+print(sorted(count.items()))
